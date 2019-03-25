42a5149967884adc7c3c46aba0b49d1b21772410
Patrick Kim <pkim050@gmail.com>
2019-03-25 23:21:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration
+  def change
+    create_table costume_stores do |element|
+      element.string :name
+      element.string :location
+      element.integer :costume_inventory
+      element.integer :employees
+      element.boolean :business
+      element.time :opening_time
+      element.time :closing_time
+    end
+  end
+end
