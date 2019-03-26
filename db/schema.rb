3cef1bc702d2e4061995256b70498b6844d9d556
Patrick Kim <pkim050@gmail.com>
2019-03-26 00:33:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "employees"
+    t.integer "num_of_employee"
     t.boolean "business"
     t.time "opening_time"
     t.time "closing_time"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.integer "size"
     t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
