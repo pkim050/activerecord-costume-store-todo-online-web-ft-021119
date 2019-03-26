928001f2d1d8d4211e795fc2858960fc7ffd954d
Patrick Kim <pkim050@gmail.com>
2019-03-26 00:36:04 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |element|
+      element.string :name
+      element.string :location
+      element.integer :costume_inventory
+      element.integer :num_of_employees
+      element.boolean :still_in_business
+      element.datetime :opening_time
+      element.datetime :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |element|
+      element.string :name
+      element.string :location
+      element.string :theme
+      element.float :price
+      element.boolean :family_friendly
+      element.date :opening_date
+      element.date :closing_date
+      element.string :description
+    end
+  end
+end
